f084c58ea3516312c3f94820ae7952473ef07b72
Gray Marchiori-Simpson <gray@etthinkpad.brewery>
2019-01-10 15:44:21 +0000

    Changed to GEO method for azure to allow permitting bing.

--- azure-decoder.py
+++ azure-decoder.py
@@ -2,15 +2,16 @@
 
 import argparse
 import xml.etree.ElementTree as xml
-#import json
-#import urllib.request as request
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Outputs an NGINX deny list for all Azure servers, as an anti-scraping technique.", prog="amazon-decoder", usage="azure-decoder > /etc/nginx/azure_blocklist.conf")
     _args = parser.parse_args()
     xmltree = xml.parse('PublicIPs_20190107.xml')
     xmlroot = xmltree.getroot()
-
+    print("geo $azure_blocklist {")
+    print("	default 0;")
     for region in xmlroot:
         for iprange in region:
-            print("deny " + iprange.attrib["Subnet"] + ";")
+            print("	" + iprange.attrib["Subnet"] + " 1;")
+    print("}")
+
